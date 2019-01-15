3053efba20c052786fcb0d948b8cd98e0e610949
Rraph <raphael.beghin@ynov.com>
2019-01-15 17:02:02 +0100

    dico fini

--- lib/pyforce/dict/__init__.py
+++ lib/pyforce/dict/__init__.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/env python3
 
+from pyforce.commons import ENCRYPTIONS
+import sys
+import time
 
-def dict_attack(password_hash, dict_path, max_size, encryption):
+def dict_attack(password_hash, dict_path, encryption):
     with open(dict_path, "rU") as read_file:
-        tested_hash = ENCRYPTIONS[encryption](
-            tested_string.encode('utf-8')).hexdigest()
-        if tested_string == row:
-            print('$$ GG Password is: "{}"'.format(tested_string))
-            print('$$ Finished at: {}'.format(time.strftime('%H:%M:%S')))
-            sys.exit(0)
-    read_file.closed
-    print('password not found')
-    sys.exit(1)
+        for row in read_file:
+            row = row.split('\n')[0]
+            tested_hash = ENCRYPTIONS[encryption](
+                row.encode('utf-8')).hexdigest()
+            if tested_hash == password_hash:
+                print('$$ GG Password is: "{}"'.format(row))
+                print('$$ Finished at: {}'.format(time.strftime('%H:%M:%S')))
+                sys.exit(0)
+        read_file.closed
+        print('password not found')
+        sys.exit(1)
